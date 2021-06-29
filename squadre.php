571345f7fc7dc8c2ef6db08e86e1e39b7ad34183
marcocavezza <86656789+marcocavezza@users.noreply.github.com>
2021-06-29 18:32:38 +0200

    vamos

--- squadre.php
+++ squadre.php
@@ -12,19 +12,22 @@
         <div class="column_middle">
             <h2> Squadre </h2>
             <h3>Serie A</h3>
-            <img id="logomilan" src="./assets/images/milan.png" alt="logo milan" width="84" height="84"/>
-            <img id="logointer" src="./assets/images/inter.png" alt="logo inter" width="84" height="84"/>
-            <img id="logoroma" src="./assets/images/roma.png" alt="logo roma" width="84" height="84"/>
+            <img id="logomilan" src="./assets/images/milan.png" alt="logo milan" width="150" height="100"/>
+            <img id="logointer" src="./assets/images/inter.png" alt="logo inter" width="150" height="100"/>
+            <img id="logoroma" src="./assets/images/roma.png" alt="logo roma" width="150" height="100"/>
             <h3>Laliga Santander</h3>
-            <img id="logobarca" src="./assets/images/barca.png" alt="logo barca" width="84" height="84"/>
-            <img id="logoreal" src="./assets/images/real.png" alt="logo real" width="84" height="84"/>
+            <img id="logoreal" src="./assets/images/real.png" alt="logo real" width="150" height="100"/>
+            <img id="logobarca" src="./assets/images/barca.png" alt="logo barca" width="100" height="100"/>
             <h3>Bundesliga</h3>
-            <img id="logobayern" src="./assets/images/bayern.png" alt="logo bayern" width="84" height="84"/>
-            <img id="logodortmund" src="./assets/images/dortmund.png" alt="logo dortmund" width="84" height="84"/>
+            <img id="logobayern" src="./assets/images/bayern.png" alt="logo bayern" width="150" height="100"/>
+            <img id="logodortmund" src="./assets/images/dortmund.png" alt="logo dortmund" width="100" height="100"/>
+            <h3>Premier League</h3>
+            <img id="logounited" src="./assets/images/united.png" alt="logo united" width="100" height="100"/>
+            <img id="logoliverpool" src="./assets/images/liverpool.png" alt="logo liverpool" width="100" height="100"/>
             <h3>Ligue 1</h3>
-            <img id="logolille" src="./assets/images/lille.png" alt="logo lille" width="84" height="84"/>
+            <img id="logolille" src="./assets/images/lille.png" alt="logo lille" width="100" height="100"/>
         </div>  
-            <div class="column_right">
+        <div class="column_right">
 
         </div>
     </div>
